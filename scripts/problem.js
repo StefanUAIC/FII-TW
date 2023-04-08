c8fe1273e15d737704df8f3fd0561a800c8b87d9
AlexandruGabriel02 <aliciucalexandru@yahoo.com>
2023-04-09 00:34:37 +0300

    implemented script for popup screen in the teacher problem page

--- scripts/problem.js
+++ scripts/problem.js
@@ -1,3 +1,4 @@
+//pentru code editor
 let editor = document.querySelector("#editor");
 
 ace.edit(editor, {
@@ -8,3 +9,23 @@ ace.edit(editor, {
     enableLiveAutocompletion: true,
     enableBasicAutocompletion: true,
 });
+
+
+//pop-up pentru profesorul corector din pagina de probleme
+var modal = document.getElementsByClassName("modal-container")[0];
+var assignBtn = document.getElementById("assign-homework");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+assignBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
