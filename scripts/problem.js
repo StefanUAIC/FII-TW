106ab19d61f8704db88ff614e364728d3ad946b0
AlexandruGabriel02 <aliciucalexandru@yahoo.com>
2023-04-09 19:44:29 +0300

    bugfix

--- scripts/problem.js
+++ scripts/problem.js
@@ -6,10 +6,7 @@ ace.edit(editor, {
     selectionStyle: "text",
     behavioursEnabled: false,
     fontSize: "16px",
-    enableLiveAutocompletion: true,
-    enableBasicAutocompletion: true,
 });
-
 
 //pop-up pentru profesorul corector/elev in pagina de probleme
 var modal = document.getElementsByClassName("modal-container")[0];
@@ -43,7 +40,12 @@ window.onclick = function(event) {
 var value = document.querySelector("#grade-value")
 var input = document.querySelector("#grade-range")
 
-value.textContent = input.value;
-input.addEventListener("input", (event) => {
-  value.textContent = event.target.value;
-})
+if (value != null) {
+  value.textContent = input.value;
+}
+
+if (input != null) {
+  input.addEventListener("input", (event) => {
+    value.textContent = event.target.value;
+  })
+}
