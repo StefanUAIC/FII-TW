4631faa8cdcb2afaae56da3670850e6ee6df3bc9
AlexandruGabriel02 <70471765+AlexandruGabriel02@users.noreply.github.com>
2023-06-13 01:02:38 +0300

    refactor code

--- back-end/view/login.view.js
+++ back-end/view/login.view.js
@@ -1,9 +1,18 @@
+const getFileContent = require("../util/getFileContent.util");
+const respondFile = require("../util/getFileContent.util");
 
-const respondFile = require("../util/respondFile.util");
-
+const LOGIN_VIEW_PATH = "./view/template/index.html";
 
 const handleLoginView = (req, res) => {
-    respondFile(req, res, "index.html");
+    getFileContent(LOGIN_VIEW_PATH)
+        .then((data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        })
+        .catch((err) => {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end(err.message);
+        });
 };
 
 module.exports = handleLoginView;
