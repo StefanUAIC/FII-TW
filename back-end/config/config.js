2abf8c803d7ed8645d78c010fb076d850ffe0e44
Gabriel Laza <gabriellaza02@gmail.com>
2023-06-14 17:53:56 +0300

    database connection

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -1,4 +1,9 @@
 module.exports.config = {
     HOST: process.env.HOST || "localhost",
     PORT: process.env.PORT || 8081,
+    DB_USER: process.env.DB_USER || "user",
+    DB_PASSWORD: process.env.DB_PASSWORD || "user",
+    DB_HOST: process.env.DB_HOST || "localhost",
+    DB_PORT: process.env.DB_PORT || 27017,
+    DB_DATABASE: process.env.DB_DATABASE || "testdb"
 };
