37292a7b5812b9d6127b8476d6b62570882b023c
Gabriel Laza <gabriellaza02@gmail.com>
2023-06-16 09:46:15 +0300

    fix: password must have a least one lowercase letter

--- back-end/controller/authenticationController.js
+++ back-end/controller/authenticationController.js
@@ -82,10 +82,10 @@ const authenticationController = (req, res) => {
                     return;
                 }
 
-                const passwordRegEx = /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9]).{8,30}$/;
+                const passwordRegEx = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$&*])(?=.*[0-9]).{8,30}$/;
                 if (!passwordRegEx.test(password)) {
                     res.writeHead(400, {'Content-Type': 'application/json'});
-                    res.end(JSON.stringify({message: 'Password must have at least one special character, one digit, one uppercase letter, and be between 3 to 30 characters long'}));
+                    res.end(JSON.stringify({message: 'Password must have at least one special character, one digit, one uppercase letter, one lowercase letter, and be between 3 to 30 characters long'}));
                     return;
                 }
 
